4151fc92e9e8867484f38207b6eca46649cc5579
gibr098 <sirico.1810153@studenti.uniroma1.it>
2024-02-29 10:57:46 +0100

    touches

--- src/App.java
+++ src/App.java
@@ -20,7 +20,8 @@ import RequesterDispatcher.Requester;
 
 import java.io.*;
 
-public class App {;
+public class App {
+    ;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
@@ -52,7 +53,6 @@ public class App {;
 
         String policy = prop.getProperty("policy");
 
-
         NFVI nfvi = new NFVI("NFVI");
         NFVIPoP pop = new NFVIPoP("PoP-1");
         DataCenter dc = new DataCenter("DC-1");
@@ -63,50 +63,53 @@ public class App {;
         nfvi.insertLinkCompose(lc);
         pop.insertLinkOwn(lo);
 
-        for(int i =0; i < number_of_servers; i++){
-            COTServer si = new COTServer("Server-"+i,server_ram, server_cpu, server_storage, server_network);
+        for (int i = 0; i < number_of_servers; i++) {
+            COTServer si = new COTServer("Server-" + i, server_ram, server_cpu, server_storage, server_network);
             LinkContain lci = new LinkContain(dc, si);
             dc.insertLinkContain(lci);
-            for(int j = 0; j < number_of_containers; j++){
-                Container cij = new Container("Container-"+i +j, container_ram, container_cpu, container_storage, container_network, container_cpu_usage);
+            for (int j = 0; j < number_of_containers; j++) {
+                Container cij = new Container("Container-" + i + j, container_ram, container_cpu, container_storage,
+                        container_network, container_cpu_usage);
                 LinkInstance lij = new LinkInstance(si, cij);
                 si.insertLinkInstance(lij);
             }
         }
 
-        //PRINT The structure of the NFVI
+        // PRINT The structure of the NFVI
         printPoPStructure(nfvi);
-        
 
+        System.out.println("Press \"ENTER\" to run the simulation...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
 
-        //Run the Simulation
+        // Run the Simulation
         AppRS app = new AppRS(lambda, duration, pop);
         app.run();
 
-        //TODO: PRINT The result of the Simulation ???
+        // TODO: PRINT The result of the Simulation ???
         printSimulationResults();
 
     }
 
-    public static void printPoPStructure(NFVI n) throws Exception{
+    public static void printPoPStructure(NFVI n) throws Exception {
         NFVIPoP pop = n.getLinkCompose().iterator().next().getNFVIPoP();
         DataCenter dc = pop.getLinkOwn().getDataCenter();
-        String info = "Construction of "+n.getName()+" with:\n";
-        info+="Point of Presence "+pop.getName()+" that has 1 Data Center "+ dc.getName()+"\n";
-        info+=dc.getName()+" has "+dc.getLinkContain().size()+" servers: \n";
-        for (LinkContain lc : dc.getLinkContain()){
+        String info = "Construction of " + n.getName() + " with:\n";
+        info += "Point of Presence " + pop.getName() + " that has 1 Data Center " + dc.getName() + "\n";
+        info += dc.getName() + " has " + dc.getLinkContain().size() + " servers: \n";
+        for (LinkContain lc : dc.getLinkContain()) {
             COTServer s = lc.getCOTServer();
-            info+= " "+s.getTotalResourcesInfo()+" has:\n";
-            for(LinkInstance li : s.getLinkInstance()){
+            info += " " + s.getTotalResourcesInfo() + " has:\n";
+            for (LinkInstance li : s.getLinkInstance()) {
                 Container c = li.getContainer();
-                info+="   "+c.getTotalResourcesInfo()+"\n";
+                info += "   " + c.getTotalResourcesInfo() + "\n";
             }
         }
 
         System.out.println(info);
     }
 
-    public static void printSimulationResults(){
+    public static void printSimulationResults() {
 
     }
 }
